567d4ef6404c132c9346dc713e05a5c092b6a982
jwon114 <jthgwong@gmail.com>
2017-02-27 17:09:46 +1100

    added with for file close

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import os;
 import shutil;
 
-dst = 'D:\Python\Python_file_copy\Output';
-src = 'D:\Python\Python_file_copy\Files';
+dst = 'C:\Python\python_file_copy\Output';
+src = 'C:\Python\python_file_copy\Files';
 
-f = open('list.txt', 'r')
-
-for line in f:
-	for filename in os.listdir(src):
-		# use rstrip() to remove \n and whitespace.
-		if line.rstrip() == filename:				
-			print('yes ' + filename)			
-			shutil.copy(src + '\\' + filename, dst + '\\' + filename);
+with open('list.txt', 'r') as f:
+	for line in f:
+		for filename in os.listdir(src):
+			# use rstrip() to remove \n and whitespace.
+			if line.rstrip() == filename:				
+				print(filename + ' Copied to Output directory')			
+				shutil.copy(src + '\\' + filename, dst + '\\' + filename);
+			
