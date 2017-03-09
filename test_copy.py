6abcadf5b66d1d223ea66fc41bac908f18c18bbd
jwon114 <jthgwong@gmail.com>
2017-03-09 14:18:18 +1100

    re-wrote copy algorithm, added test file create

--- test_copy.py
+++ test_copy.py
@@ -7,11 +7,13 @@ dst = 'C:\\Python\\python_file_copy\\Test Output\\'
    
 with open('list_test.txt', 'r') as list_test:
     print('Opening list file for reading')
-    for filename in list_test:
-        filename = filename.rstrip() + '.xml'
-        if os.path.isfile(src + filename):
-            print(filename + ' File found, copying to new location')
-            shutil.copy(src + filename, dst + filename);
+    with open('log_test.txt', 'a', 1) as log:
+        for filename in list_test:
+            filename = filename.rstrip() + '.xml'
+            if os.path.isfile(src + filename):
+                print(filename + ' File found, copying to new location')
+                log.write(filename + ' file found and copying to output location\n')
+                shutil.copy(src + filename, dst + filename);
             
 end = time.time()
 print(end - start)
