493068288bd2cc24b94cc11ae182d6f1191c6611
jwon114 <jthgwong@gmail.com>
2017-04-03 11:00:39 +1000

    removed and renamed file_copy.py

--- file_copy.py
+++ file_copy.py
@@ -1,20 +1,21 @@
-import os, shutil, time;
+import os, shutil, time
 
 start = time.time()
 
-dst = 'C:\\Python\\python_file_copy\\Test Output';
-src = 'C:\\Python\\python_file_copy\\Test Files';
-#dst = 'C:\Slainte_Data\Python\python_file_copy\Output';
-#src = 'L:\\';
-
-with open('list_test.txt', 'r') as f:
-	print('Opened file successfully')
-	for line in f:
-		for filename in os.listdir(src):
-			# use rstrip() to remove \n and whitespace.
-			if line.rstrip() == filename.replace('.xml', ''):				
-				print(filename + ' Copied to Output directory')			
-				shutil.copy(src + '\\' + filename, dst + '\\' + filename);
-				
+src = 'C:\\Python\\python_file_copy\\Test Files\\';
+dst = 'C:\\Python\\python_file_copy\\Test Output\\'
+   
+with open('list.txt', 'r') as list_test:
+    print('Opening list file for reading')
+    with open('log.txt', 'a', 1) as log:
+        for filename in list_test:
+            print('Searching for file ' + filename)
+            log.write('Searching for file ' + filename + '\n')
+            filename = filename.rstrip() + '.xml'
+            if os.path.isfile(src + filename):
+                print(filename + ' File found, copying to new location')
+                log.write(filename + ' File found, copying to new location\n')
+                shutil.copy(src + filename, dst + filename);                
+            
 end = time.time()
 print(end - start)
