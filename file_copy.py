5fb1d15d6202d503ada955cbf6ef57e042a3d68a
jwon114 <jthgwong@gmail.com>
2017-04-03 14:26:59 +1000

    open from csv, error log

--- file_copy.py
+++ file_copy.py
@@ -1,21 +1,22 @@
-import os, shutil, time
+import os, shutil, time, csv
 
 start = time.time()
 
-src = 'C:\\Python\\python_file_copy\\Test Files\\';
+src = 'C:\\Python\\python_file_copy\\Test Files\\'
 dst = 'C:\\Python\\python_file_copy\\Test Output\\'
    
-with open('list.txt', 'r') as list_test:
+with open('file_copy.csv', 'r', newline='') as file_csv:
     print('Opening list file for reading')
     with open('log.txt', 'a', 1) as log:
-        for filename in list_test:
-            print('Searching for file ' + filename)
-            log.write('Searching for file ' + filename + '\n')
-            filename = filename.rstrip() + '.xml'
+        results = csv.reader(file_csv, delimiter=',')
+        for line in results:
+            filename = line[1].rstrip() + '.xml'            
             if os.path.isfile(src + filename):
                 print(filename + ' File found, copying to new location')
                 log.write(filename + ' File found, copying to new location\n')
-                shutil.copy(src + filename, dst + filename);                
+                shutil.copy(src + filename, dst + filename);
+            else:
+                log.write('ERROR! File ' + filename + ' not found\n')
             
 end = time.time()
 print(end - start)
