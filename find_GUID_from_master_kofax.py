082c64097ce2d00daf26bf492c402d7586b7b15d
jwon114 <jthgwong@gmail.com>
2017-04-06 10:26:55 +1000

    find_guid updates

--- find_GUID_from_master_kofax.py
+++ find_GUID_from_master_kofax.py
@@ -1,3 +1,6 @@
+# Using the difference in mrn_ep keys from Vitro DB and the matching result set, find the data [mrn_ep, guid, dt]
+# from the KOFAX drive extraction [mrn_ep, guid, last_modified_dt]
+
 import csv
 
 #SRC1 = 'C:\\Slainte_Data\\Python\\python_file_copy\\vndocsprd_output.txt';
@@ -9,9 +12,11 @@ DST = 'C:\\Slainte_Data\\Python\\python_file_copy\\mrn_ep_missing_from_results_i
 results_set = []
 vndocsprod_set = []
 
+# Missing results set (Vitro DB - matching result) [mrn_ep]
 with open(SRC2, 'r', 1) as missingResults:
 	results_set = [line.strip() for line in missingResults]
-	
+
+# Create key set [mrn_ep] and data set [mrn_ep, guid, dt]
 with open(SRC1, 'r', newline='') as vndocsprd:
 	vndocsprd_in = csv.reader(vndocsprd, delimiter=',')	
 	vndocsprd_setData = []
@@ -23,19 +28,11 @@ with open(SRC1, 'r', newline='') as vndocsprd:
 with open(DST, 'a', 1) as missingOutput:
 	for key in results_set:
 		if key in vndocsprd_set:
-			vndocsprd_indices = [i for i, x in enumerate(vndocsprd_set) if x == key]
-			print(vndocsprd_indices)
+			vndocsprd_indices = [index for index, value in enumerate(vndocsprd_set) if value == key]						
 			for index in vndocsprd_indices:
-				print(vndocsprd_setData[index])
-			del vndocsprd_set[vndocsprd_set.index(key)]
-	
-
-#print(results_set)
-#print(vndocsprd_setData)
-#for key in vndocsprd_setData:	
-#	vndocsprd_indices = [i for i, x in enumerate(vndocsprd_setData)]
-#print(vndocsprd_set)
-#print(vndocsprd_indices)
-
-
+				missingOutput.write(','.join(map(str,(vndocsprd_setData[index])))+'\n')								
+			
+			# Remove found values from the key set and data set for repeat searches
+			vndocsprd_set = [value for index, value in enumerate(vndocsprd_set) if index not in vndocsprd_indices]
+			vndocsprd_setData = [value for index, value in enumerate(vndocsprd_setData) if index not in vndocsprd_indices]
 			
